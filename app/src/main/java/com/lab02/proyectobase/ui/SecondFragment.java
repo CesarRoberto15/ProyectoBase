6fcd51623224e2f14d8d253620d289f277c10142
gelber12345 <gmartinezpas@unsa.edu.pe>
2021-11-19 13:18:33 -0500

    "Presentacion"

--- app/src/main/java/com/lab02/proyectobase/ui/SecondFragment.java
+++ app/src/main/java/com/lab02/proyectobase/ui/SecondFragment.java
@@ -2,12 +2,20 @@ package com.lab02.proyectobase.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.lab02.proyectobase.R;
 
 /**
@@ -62,5 +70,32 @@ public class SecondFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+    //ubicacion del mapa en la aplicacion
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng sydney = new LatLng(-16.404815474841488, -71.52656651318283); //-16.404815474841488, -71.52656651318283
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("UNSA Ingenierias"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+        }
+    };
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapAlbergue);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
     }
 }
